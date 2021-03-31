ffc3f620d37e12411de5685925bc9084f72cb749
Kendall <ktrudickm@gmail.com>
2021-03-31 12:41:01 -0400

    Added class Manager to Manager file to extend Employee and added office number via super & constructor. Added getOffice and getRole

---
+++ Develop/lib/Manager.js
@@ -0,0 +1,20 @@
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+
+    constructor(name, id, email, officeNum) {
+        super(name, id, email);
+        this.officeNum = officeNum;
+    }
+
+    getOffice() {
+        return this.officeNum;
+    };
+
+    getRole() {
+        return "Manager";
+    };
+
+}
+
+module.exports = Manager;
