440b30db2746f4eae862e2a089925f8058efbb7d
Kendall <ktrudickm@gmail.com>
2021-03-31 12:07:00 -0400

    Completed Engineer test

--- Develop/Tests/Engineer.test.js
+++ Develop/Tests/Engineer.test.js
@@ -1,7 +1,19 @@
 const engineer = require("../lib/Engineer");
 
-test("Can set Engineer's GitHUb account using constructor", () => {
+test("Can set Engineer's GitHub account using constructor", () => {
     const gitHubName = "GitHubUser";
     const obj = new engineer("Engineer", 1, "testemail@email.com", gitHubName);
     expect(obj.github).toBe(gitHubName);
+});
+
+test("getRole() for engineer should return \"Engineer\"", () => {
+    const role = "Engineer";
+    const obj = new engineer("Role", 1, "testemail@email.com", "GitHubUser");
+    expect(obj.getRole()).toBe(role);
+});
+
+test("Can get GitHub username through getGithub()", () => {
+    const gitHubName = "GitHubUser";
+    const obj = new engineer("Role", 1, "testemail@email.com", gitHubName);
+    expect(obj.getGithub()).toBe(gitHubName);
 });
