d9ebec7f7ca06212682b8b7c0fe4382f5620f931
Kendall <ktrudickm@gmail.com>
2021-03-31 09:48:20 -0400

    Fixed gramatical errors in intern test

--- Develop/Tests/Intern.test.js
+++ Develop/Tests/Intern.test.js
@@ -2,7 +2,7 @@ const intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
     const School = "UCLA";
-    const obj = new Intern("Foo", 1, "testemail@email.com", School);
+    const obj = new intern("Foo", 1, "testemail@email.com", School);
     expect(obj.school).toBe(School);
 });
 
