7d6a4c179afa6a2fdbca00e19395c176a948c0ad
Kendall <ktrudickm@gmail.com>
2021-03-31 12:24:28 -0400

    Worked on Employee.js file adding getID, getName, getEmail, getRole and adding constructor

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,32 @@
+class Employee {
+
+    constructor(name, id, email) {
+        if (typeof name !== "string" || !name.trim().length) {
+            throw new Error("Expected 'name' parameter to be a type of string, and not empty.")
+        }
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    };
+
+    getId() {
+        return this.id;
+    };
+
+    getEmail() {
+        return this.email;
+    };
+
+    getRole() {
+        return "Employee";
+    };
+
+
+}
+
+module.exports = Employee;
