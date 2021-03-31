bc752d47e1e6fce0f22d1b4d71f814d199f19fb8
Kendall <ktrudickm@gmail.com>
2021-03-31 12:17:46 -0400

    Completed Manager tests

--- Develop/Tests/Manager.test.js
+++ Develop/Tests/Manager.test.js
@@ -1,1 +1,20 @@
 const manager = require("../lib/Manager");
+const employee = require("../lib/Employee");
+
+test("Can set Managers office number using constructor", () => {
+    const office = 10;
+    const obj = new manager("Role", 1, "testemail@email.com", office);
+    expect(obj.officeNum).toBe(office);
+});
+
+test("getRole() should return the role as: \"Manager\"", () => {
+    const role = "Manager";
+    const obj = new manager("Role", 1, "testemail@email.com", 10);
+    expect(obj.getRole()).toBe(role);
+});
+
+test("Use getOffice() to get the office number", () => {
+    const office = 10;
+    const obj = new manager("Role", 1, "testemail@email.com", office);
+    expect(obj.getOffice()).toBe(office);
+})
